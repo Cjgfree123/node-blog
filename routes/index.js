d8e4714c353dd75004e31a4f7b276ba4a33e6db2
jingechen <jingechen@sohu-inc.com>
2019-10-07 22:52:17 +0800

    add:关键词搜索

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,29 @@ let router = express.Router();
 
 // / [get] 首页
 router.get("/", function (req, res) {
-    Article.find().populate("user").exec(function (err, articles) {
+    let {
+        keyword
+    } = req.query;
+    let query = {};
+    if (keyword) {
+        // 标题、内容匹配关键字
+        // query.title = new RegExp(keyword);
+        // 正则或
+        query['$or'] = [
+            {
+                title: new RegExp(keyword)
+            }, 
+            {
+                content: new RegExp(keyword)
+            }];
+    };
+
+    Article.find(query).populate("user").exec(function (err, articles) {
         // 路由是相对路径，相对于模板根目录
         res.render("index", {
             title: "首页",
             articles,
+            keyword,
         });
     });
 });
